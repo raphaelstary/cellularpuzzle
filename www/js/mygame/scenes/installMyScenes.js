22aa80ccb0735a0409d6327db603658066fd383f
raphaelstary <raphael.stary@gmail.com>
2016-08-11 12:09:47 +0200

    fix merging issues

--- www/js/mygame/scenes/installMyScenes.js
+++ www/js/mygame/scenes/installMyScenes.js
@@ -1,14 +1,14 @@
-G.installMyScenes = (function (SceneManager, MVVMScene, Constants, StartScreen, LevelOverview, TapManager, Event) {
+G.installMyScenes = (function (Scenes, MVVMScene, Constants, StartScreen, LevelOverview, Taps, Event) {
     "use strict";
 
     function installMyScenes(sceneServices) {
         // create your scenes and add them to the scene manager
 
-        var tap = new TapManager();
+        var tap = new Taps();
         sceneServices.tap = tap;
         sceneServices.events.subscribe(Event.POINTER, tap.inputChanged.bind(tap));
 
-        var sceneManager = new SceneManager();
+        var sceneManager = new Scenes();
 
         var startScreen = new MVVMScene(sceneServices, sceneServices.scenes[Constants.START_SCREEN], new StartScreen(sceneServices), Constants.START_SCREEN);
         var levelOverview = new MVVMScene(sceneServices, sceneServices.scenes[Constants.LEVEL_OVERVIEW], new LevelOverview(sceneServices), Constants.LEVEL_OVERVIEW);
@@ -20,4 +20,4 @@ G.installMyScenes = (function (SceneManager, MVVMScene, Constants, StartScreen, 
     }
 
     return installMyScenes;
-})(H5.SceneManager, H5.MVVMScene, G.Constants, G.StartScreen, G.LevelOverview, H5.TapManager, H5.Event);
+})(H5.Scenes, H5.MVVMScene, G.Constants, G.StartScreen, G.LevelOverview, H5.Taps, H5.Event);
